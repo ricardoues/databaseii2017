248b41a0f27590ff7847f8915b9846f16a4a9f28
ricardoues <mathricardorios@gmail.com>
2017-04-07 14:34:45 -0600

    Update python001.py

--- python_postgresql/python001.py
+++ python_postgresql/python001.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 ## http://stackoverflow.com/questions/9353822/connecting-postgresql-with-sqlalchemy
 
 ## Create engine: engine
-engine = create_engine('postgresql+psycopg2://mate:dataBASE**345**@localhost/ricardodellstore')
+engine = create_engine('postgresql+psycopg2://mate:password@localhost/ricardodellstore')
 
 ## Save the table names to a list: table_names 
 table_names = engine.table_names()
